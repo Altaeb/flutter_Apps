346f9aad491afbaa6e64b115b33ba2199f7d713a
abdalfattah <abd2010ly@gmail.com>
2019-05-15 10:01:56 +0200

    named arguments

--- lib/products.dart
+++ lib/products.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products(this.products) {
+  Products([this.products = const []]) {
     print('[Products Widget] Constructor');
   }
 
@@ -16,7 +16,7 @@ class Products extends StatelessWidget {
             (element) => Card(
                   child: Column(
                     children: <Widget>[
-                      // Image.asset('assets/food.jpg'),
+                      Image.asset('assets/food.jpg'),
                       Text(element)
                     ],
                   ),
