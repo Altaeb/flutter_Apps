3622a6f31cf4a8d6862b5855f8f884afe5183087
abdalfattah <abd2010ly@gmail.com>
2019-05-15 12:38:38 +0200

    if and build method

--- lib/products.dart
+++ lib/products.dart
@@ -18,12 +18,24 @@ class Products extends StatelessWidget {
     );
   }
 
+  Widget _buildProductList() {
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(
+        child: Text('No products found, please add some'),
+      );
+    }
+    return productCard;
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-    return products.length > 0 ? ListView.builder(
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    ) : Center(child: Text('No products found, please add some'),);
+    return _buildProductList();
   }
 }
