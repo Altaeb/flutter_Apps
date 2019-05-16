f30a2693d3abc4e3a2db49bde6764063f8d48680
abdalfattah <abd2010ly@gmail.com>
2019-05-16 12:23:15 +0200

    04 Fixing a Tiny Error & Adding a Switch

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,8 @@ class _MyAppState extends State<MyApp> {
           accentColor: Colors.deepPurple),
       // home: AuthPage(),
       routes: {
-        '/': (BuildContext context) => ProductsPage(_products),
+        '/': (BuildContext context) => AuthPage(),
+        '/products': (BuildContext context) => ProductsPage(_products),
         '/admin': (BuildContext context) =>
             ProductsAdminPage(_addProduct, _deleteProduct),
       },
