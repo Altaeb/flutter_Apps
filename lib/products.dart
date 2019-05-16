3137c26c488468df45a5efcae0bf3153b1e29352
abdalfattah <abd2010ly@gmail.com>
2019-05-16 15:14:14 +0200

    widget-deep-dive-03-added-address

--- lib/products.dart
+++ lib/products.dart
@@ -40,6 +40,13 @@ class Products extends StatelessWidget {
                   )
                 ],
               )),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0)),
+            child: Text('Union Square, San Francisco'),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
