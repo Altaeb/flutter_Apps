8e579fb4085f8d1ee9c5af41008b6549b6560846
abdalfattah <abd2010ly@gmail.com>
2019-05-16 06:28:30 +0200

    navigation-05-added-side-drawer

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -6,6 +6,20 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('EasyList'),
       ),
