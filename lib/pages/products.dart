0d56d7349b360678a3754cbdac746828d19fef3c
abdalfattah <abd2010ly@gmail.com>
2019-05-16 06:45:05 +0200

    navigation-08-tabs

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../product_manager.dart';
+import './products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
@@ -15,7 +16,13 @@ class ProductsPage extends StatelessWidget {
             ),
             ListTile(
               title: Text('Manage Products'),
-              onTap: () {},
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            ProductsAdminPage()));
+              },
             )
           ],
         ),
