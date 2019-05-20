6bac0c61c3a9c293588f2bd3d60b659d8bb93950
abdalfattah <abd2010ly@gmail.com>
2019-05-20 14:53:32 +0200

    auth-08-Allow Editing for own Posts Only

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -160,7 +160,7 @@ class ProductsModel extends ConnectedProductsModel {
     });
   }
 
-  Future<Null> fetchProducts() {
+  Future<Null> fetchProducts({onlyForUser = false}) {
     _isLoading = true;
     notifyListeners();
     return http
@@ -187,7 +187,9 @@ class ProductsModel extends ConnectedProductsModel {
                 .containsKey(_authenticatedUser.id));
         fetchedProductList.add(product);
       });
-      _products = fetchedProductList;
+      _products = onlyForUser ? fetchedProductList.where((Product product) {
+        return product.userId == _authenticatedUser.id;
+      }).toList() : fetchedProductList;
       _isLoading = false;
       notifyListeners();
       _selProductId = null;
